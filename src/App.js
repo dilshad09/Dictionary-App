4fd15974e133c145c3c9d8ee4767894e831b4a96
Dilshad <dilshad_fw11_177@masai.school>
2022-04-14 23:39:51 +0530

    fetching api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 
 import './App.css';
-import Button from './components/button/Button';
 import axios from 'axios'
 import { useEffect, useState } from 'react';
 import { Container } from '@mui/material';
 import Header from './components/Header/Header';
+
 function App() {
   const [word, setWord] = useState("")
   const [category, setCategory] = useState("en")
   const [meanings, setMeanings] = useState([])
 
   const dictionaryApi = async()=>{
-  const {data} = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/love")
+  try {
+    const {data} = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`)
     setMeanings(data)
+  } catch (error) {
+    console.log(error)
+  }
   }
   useEffect(()=>{
     dictionaryApi()
-  },[])
+    console.log(meanings)
+  },[word, category])
+  
   return (
     <div className="App">
       <Header category={category} setCategory={setCategory} word={word} setWord={setWord}/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,18 @@ import React, { useState } from 'react'
 import './Header.css'
 import categories from '../data/Category'
 const Header = ({category, setCategory, word, setWord}) => {
+    const handleChange = (language)=>{
+        setCategory(language)
+        setWord("")
+    }
     
   return (
     <div className='header'>
       <div className='title'>
           <span>{word ? word : "Word Hunt"}</span>
           <div className='inputs'>
-          <TextField id="standard-basic" value={word} onChange={(e)=>setWord(e.target.value)} label="Search a word here" variant="standard" />
-          <TextField id="select" label="Language" onChange={(e)=>setCategory(e.target.value)} value={category} select>
+          <TextField id="standard-basic" value={word} onChange={(e)=>setWord(e.target.value)} label="Search" variant="standard" />
+          <TextField id="select" label="Language" onChange={(e)=>handleChange(e.target.value)} value={category} select>
           {
               categories.map((options, i)=>{
                return <MenuItem value={options.label} key={i}>{options.value}</MenuItem>
