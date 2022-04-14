d9caab7ad702a3d08b2eb89c6431b3820ac35713
Dilshad <dilshad_fw11_177@masai.school>
2022-04-14 20:45:41 +0530

    fetched first api with google dictionary api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 
 import './App.css';
 import Button from './components/button/Button';
+import axios from 'axios'
+import { useEffect } from 'react';
 function App() {
+
+  const dictionaryApi = async()=>{
+    const data = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/love")
+    console.log(data)
+  }
+  useEffect(()=>{
+    dictionaryApi()
+  },[])
   return (
     <div className="App">
      <Button/>
